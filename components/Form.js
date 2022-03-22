91a582de1e6862c51647d9b632e74b32830b01c7
Azamat Rsymbetov <seetwopluses@gmail.com>
2022-03-22 23:18:56 +0600

    Fixed design mismatch

--- components/Form.js
+++ components/Form.js
@@ -2,11 +2,14 @@ import TextField from "./TextField";
 
 function Form({ label, children }) {
   return (
-    <div className="flex flex-col items-center p-3 bg-white w-1/2 m-10 rounded-2xl shadow font-sans">
-      <h1 className="text-2xl">{label} Form</h1>
-      {children}
-      <div className="flex flex-col items-center w-full bg-blue-300 text-white w-full p-3 rounded-2xl text-2xl m-4 hover:opacity-90">
-        <a href="#">{label}</a>
+    <div className="flex flex-col items-center px-4 py-6 bg-white w-[448px] rounded-2xl shadow font-sans">
+      <div className="flex flex-col w-full h-full items-center">
+        <h1 className="text-3xl">{label}</h1>
+        <br/>
+        {children}
+        <div className="flex flex-col items-center w-full bg-blue-500 font-[Gilroy-Light] text-2xl text-white rounded-2xl hover:opacity-80 py-4 mt-10">
+          <a href="#">{label}</a>
+        </div>
       </div>
     </div>
   )
@@ -15,10 +18,10 @@ function Form({ label, children }) {
 export function SignIn() {
   return (
     <Form label="Sign In">
-      <TextField label="username"/>
-      <TextField label="password" password />
-      <div className="w-full p-1">
-        <a href="#" className="font-medium text-md">forgot password?</a>
+      <TextField label="Username"/>
+      <TextField label="Password" password />
+      <div className="w-full px-1 pt-4">
+        <a href="#" className="font-medium text-md">Forgot password?</a>
       </div>
     </Form>
   )
@@ -27,10 +30,10 @@ export function SignIn() {
 export function SignUp() {
   return (
     <Form label="Sign Up">
-      <TextField label="username"/>
-      <TextField label="email"/>
-      <TextField label="password" password />
-      <TextField label="repeat password" password />
+      <TextField label="Username"/>
+      <TextField label="Email"/>
+      <TextField label="Password" password />
+      <TextField label="Repeat password" password />
     </Form>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -39,7 +39,7 @@ const MyLink = ({secondary, children, to= "#"}) => {
 
 function Navbar() {
   return (
-    <nav className="flex justify-between px-10 py-2 bg-white w-full">
+    <nav className="flex justify-between px-20 py-2 bg-white w-full">
       <Link href='/'><a><Logo/></a></Link>
       <div className="flex px-2 space-x-4">
         <MyLink secondary to="/signUp">Sign Up</MyLink>
