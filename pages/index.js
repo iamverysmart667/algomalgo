a6036bc37464172da7118442b437efe4eb1de868
Azamat Rsymbetov <seetwopluses@gmail.com>
2022-03-22 13:11:56 +0600

    Added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Navbar from "../components/Navbar";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h1 className="underline text-3xl font-bold">
-      Hello world!
-    </h1>
+      <>
+      <Head>
+          <title>AlgoMalgo!</title>
+          <link href="http://fonts.cdnfonts.com/css/gilroy-bold?styles=20876,20878,20879,20880" rel="stylesheet"/>
+      </Head>
+      <div className="bg-sky-100">
+          <Navbar/>
+          <h1 className="underline text-3xl font-bold">
+              Hello world!
+          </h1>
+      </div>
+      </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ["Gilroy-Medium", "Consolas", "Sans"]
+    }
   },
   plugins: [],
 }
