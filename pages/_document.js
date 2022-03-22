b5564824430d5b9e7942e8244bf4cc209d2b332e
Azamat Rsymbetov <seetwopluses@gmail.com>
2022-03-22 13:16:49 +0600

    Added _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>AlgoMalgo!</title>
+                <link href="http://fonts.cdnfonts.com/css/gilroy-bold?styles=20876,20878,20879,20880" rel="stylesheet"/>
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    )
+}
+
+export default Document;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import Navbar from "../components/Navbar";
-import Head from "next/head";
 
 export default function Home() {
   return (
       <>
-      <Head>
-          <title>AlgoMalgo!</title>
-          <link href="http://fonts.cdnfonts.com/css/gilroy-bold?styles=20876,20878,20879,20880" rel="stylesheet"/>
-      </Head>
       <div className="bg-sky-100">
           <Navbar/>
           <h1 className="underline text-3xl font-bold">
